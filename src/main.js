401fb24dcf36d15d1c9dc91860e2e92d4ddd90ae
Jesus Alvarado Reyes <jesusar.dev@gmail.com>
2022-07-13 19:10:21 -0500

    add: axios, bootstrap, calendar and sweetalert

--- src/main.js
+++ src/main.js
@@ -3,4 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import VCalendar from 'v-calendar'
+import 'v-calendar/dist/style.css'
+
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
+
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.use(VCalendar)
+app.use(VueSweetalert2)
+app.mount('#app')

8abc8f41dfbdbd8ddaa2c52b459ced1d62e05c18
Jesus Alvarado Reyes <jesusar.dev@gmail.com>
2022-08-11 18:25:12 -0500

    feat: interns table style

--- src/components/InternList.vue
+++ src/components/InternList.vue
@@ -1,36 +1,40 @@
 <template>
-<div class="d-flex align-items-center">
-        <table class="table" align="middle">
-          <thead>
-      <tr>
-        <th scope="col">Photo</th>
-        <th scope="col">Name</th>
-        <th scope="col">Email</th>
-        <th scope="col">CV</th>
-      </tr>
-    </thead>
-          <tbody>
-            <tr v-for="intern in interns" :key="intern.ID_INTERN">
-            <td>
-              <img src="https://mdbootstrap.com/img/new/avatars/8.jpg" alt="" style="width: 50px; height: px"
-              class="rounded-circle" />
-            </td>
-              <td>
-                <p>{{ intern.FIRST_NAME }}</p>
-                <a :href="intern.PHONE_NUMBER">{{ intern.PHONE_NUMBER }}</a>
-              </td>
-              <td>{{ intern.EMAIL }}</td>
-              <td>
-                <a :href="intern.CV">
-                  <i class="fas fa-cloud-download"></i>
-                </a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-
-        <pagination :data="InternList" @pagination-change-page="InternsService"></pagination>
-      </div>
+  <div class="d-flex align-items-center table-responsive">
+    <table class="table" align="middle">
+      <thead>
+        <tr>
+          <th scope="col">N°</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">CV</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="intern in interns" :key="intern.ID_INTERN" class="fs-5">
+          <td>
+            <p class="mb-0 text-black">{{ intern.ID_INTERN }}</p>
+          </td>
+          <td>
+            <p class="mb-0 text-black">{{ intern.FIRST_NAME }}</p>
+            <a :href="`https://wa.me/${intern.PHONE_NUMBER}`" target="_blank" class="text-black text-decoration-none">
+              {{ intern.PHONE_NUMBER }}
+            </a>
+          </td>
+          <td class="pt-3">
+            <a :href="`mailto:${intern.EMAIL}`" target="_blank" class="text-black text-decoration-none">
+              {{ intern.EMAIL }}
+            </a>
+          </td>
+          <td class="pt-3">
+            <a :href="intern.CV" target="_blank">
+              <i class="fas fa-cloud-download"></i>
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- <pagination :data="InternList" @pagination-change-page="InternsService"></pagination> -->
+  </div>
 </template>
 
 <script>
@@ -45,6 +49,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.text-black {
+  color:#232527 !important;
+}
 </style>
